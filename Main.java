dcef3daa6f78d8799a3273cf394da36829f96046
Clara Akhlaghi <>
2021-04-09 01:08:05 +0000

    example that prints out all the odds of an array

--- Main.java
+++ Main.java
@@ -1,14 +1,19 @@
 class Main {
   public static void main(String[] args) {
-    int[] key = {1,1,1,1,1,1,1,1};
-    System.out.print(sumArray(key));
-  }
-  public static int sumArray(int[] key) { 
-    int sum = 0; 
-    for (int i = 1; i <= key.length; i++) { 
-      sum += key[i-1];
-    } 
-    return sum; 
+    int[] nums= {5, 1, -1, 44, 37, 19};
+    int i = 0;
+    int[] o = new int[nums.length];   
+    for (int count = 0; count < nums.length; count++) {
+      if(nums[count] % 2 != 0) {
+        o[i] = nums[count];
+        i++;
+      }
+    }
+    int[] odds= new int[i];
+    for (int j = 0; j < o.length-1; j++){
+      odds[j] = o[j];
+      System.out.println(odds[j]);
+    }   
   }
 }
 
