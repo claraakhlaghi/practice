293af8f26e0e969d2022269f0f105dd9add8cc4e
Clara Akhlaghi <>
2021-04-09 01:05:48 +0000

    example of sum method

--- Main.java
+++ Main.java
@@ -1,19 +1,14 @@
 class Main {
   public static void main(String[] args) {
-  int[] nums= {5, 1, -1, 44, 37, 19};
-  int i = 0;
-  int[] o = new int[nums.length];   
-  for (int count = 0; count < nums.length; count++) {
-    if(nums[count] % 2 != 0) {
-      o[i] = nums[count];
-      i++;
-    }
+    int[] key = {1,1,1,1,1,1,1,1};
+    System.out.print(sumArray(key));
   }
-int[] odds= new int[i];
-for (int j = 0; j < o.length-1; j++){
-  odds[j] = o[j];
-  System.out.println(odds[j]);
-}
+  public static int sumArray(int[] key) { 
+    int sum = 0; 
+    for (int i = 1; i <= key.length; i++) { 
+      sum += key[i-1];
+    } 
+    return sum; 
   }
 }
 
